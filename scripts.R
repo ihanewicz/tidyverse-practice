3178d6911d1adeed035a7df1f922f239a606b353
Isabel Hanewicz <isabel.hanewicz@sorensonimpact.com>
2021-02-11 12:34:57 -0700

    Ch. 6 problems

---
+++ scripts.R
@@ -0,0 +1,18 @@
+# scripts.R
+# Practice problems, Ch. 6 of R for Data Science
+# Isabel Hanewicz
+# 11 Feb 2021
+
+# Libraries
+library(tidyverse)
+library(dplyr)
+
+# Notes
+# CMD + SHIFT + S => execute whole script
+
+# 1 
+# control + option + k selects all instances of highlighted word
+
+# 2 
+# Examples of errors:
+# Not including all the arguments in a function call
